ff8025c7350f1ec3c131c2f2aac31f2037d354ec
rithesh48 <ritheshdj889@gmail.com>
2025-03-28 10:55:28 +0530

    Add post and user controllers with types for CRUD operations

---
+++ src/controllers/users/users-types.ts
@@ -0,0 +1,13 @@
+import type { User } from "@prisma/client";
+export type GetMeResult = {
+  user: Omit<User, "password">;
+};
+export enum GetMeError {
+  BAD_REQUEST,
+}
+export type GetAllUsersResult = {
+  users: Omit<User, "password">[];
+};
+export enum GetAllUsersError {
+  BAD_REQUEST,
+}
