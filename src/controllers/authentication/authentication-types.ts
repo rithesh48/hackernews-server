3fb6b11e251040e213446231a30f77c0d68b144d
rithesh48 <ritheshdj889@gmail.com>
2025-03-28 10:49:12 +0530

    Add authentication controller and types for user sign-up and login

---
+++ src/controllers/authentication/authentication-types.ts
@@ -0,0 +1,17 @@
+import type { User } from "@prisma/client";
+export type SignUpWithUsernameAndPasswordResult = {
+  token: string;
+  user: User;
+};
+export enum SignUpWithUsernameAndPasswordError {
+  CONFLICTING_USERNAME = "CONFLICTING_USERNAME",
+  UNKNOWN = "UNKNOWN",
+}
+export type LogInWithUsernameAndPasswordResult = {
+  token: string;
+  user: User;
+};
+export enum LogInWtihUsernameAndPasswordError {
+  INCORRECT_USERNAME_OR_PASSWORD = "INCORRECT_USERNAME_OR_PASSWORD",
+  UNKNOWN = "UNKNOWN",
+}
